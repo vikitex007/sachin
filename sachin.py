9b1f60916861f0daf794f70df125dcd7579f532c
sachin shrestha <sachecka1176@gmail.com>
2022-08-16 12:05:18 +0545

    hello

--- sachin.py
+++ sachin.py
@@ -15,5 +15,37 @@ a = 4
 
 messege = 'Hello'
 
+#  creation of function and calling of function
+
+'''
+
+syntax :
+    
+#creating function
+def function name():
+    #you can do anything here 
+    return ..... something
+
+#calling function
+function_name()
+
+'''
+
+#example:
+
+#creating function 
+def duita_num_joda():
+    sum = 3+5
+    print(sum)
+    return sum
+
+#calling function with their name
+duita_num_joda()
+
+
+#taking input from user:
+my_name=input("please enter your password: ")
+print("Ma sanga stored bhako value yei ho "),my_name
+
 
 
