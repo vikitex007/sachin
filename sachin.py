469a701dab085375a7a34aacabf1c81f28e72a23
sachin shrestha <sachecka1176@gmail.com>
2022-08-18 21:52:39 +0545

    sachin

--- sachin.py
+++ sachin.py
@@ -42,10 +42,58 @@ def duita_num_joda():
 #calling function with their name
 duita_num_joda()
 
-
 #taking input from user:
-my_name=input("please enter your password: ")
-print("Ma sanga stored bhako value yei ho "),my_name
+#my_name=input("please enter your password: ")
+#print("Ma sanga stored bhako value yei ho ",my_name)
+
+
+...
+
+
+
+
+
+first_name=input("firstname") 
+second_name=input("secondname") 
+
+def full_name_generator(first_name,second_name):
+    full_name = f"{first_name} {second_name}"
+    return full_name
+
+print("your full name is:", full_name_generator(first_name,second_name))
+
+#string formatting
+print("my-1 name-2 is-3 abc-4 def-5")
+
+phone_num = 9810119909
+print(f"your phone number is {phone_num}")
+
+
+first=1
+second=2
+third=3
+fourth=4
+
+ 
+print(f"my-{first} name-{second} is-{third} Sachin-{fourth}")
+
+
+#len function
+length_of_this = len("Education")
+print(length_of_this)
+
+length_of_this = len("junkiri")
+print(length_of_this)
+
+
+
+
+
+
+
+
+
+
 
 
 
