1c680baa34eda247b751f4464f6ec88c59e8a421
sachin shrestha <sachecka1176@gmail.com>
2022-08-31 11:50:24 +0545

    changes

---
+++ name.py
@@ -0,0 +1,48 @@
+ 
+
+
+
+first_name=input("junkiri") 
+second_name=input("kxa") 
+
+def full_name_generator(first_name,second_name):
+    full_name = f"{first_name}  {second_name}"
+    return full_name
+
+print("your full name is:", full_name_generator(first_name,second_name))
+
+#string formatting
+print("my-1 name-2 is-3 abc-4 def-5")
+
+phone_num = 9810119909
+print(f"your phone number is {phone_num}")
+
+
+first=1
+second=2
+third=3
+fourth=4
+
+ 
+print(f"my-{first} name-{second} is-{third} Sachin-{fourth}")
+
+
+#len function
+length_of_this = len("Education")
+print(length_of_this)
+
+length_of_this = len("junkiri")
+print(length_of_this)
+
+
+hello there are some changes done 
+
+
+
+
+
+
+
+
+
+
